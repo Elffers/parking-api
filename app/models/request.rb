c126eb131b831040a37120c495d7d251761ca5be
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-03 11:00:44 -0700

    get request to seattle api

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,6 +7,10 @@ class Request
   field :overlay, type: String
 
   def get_overlay
-    coords = self.coords
+    bounds = {"bbox"=>self.bounds}.to_query
+    image = HTTParty.get("http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export?#{bounds}&bboxSR=4326&layers=7&layerdefs=&size=&imageSR=&format=png&transparent=false&dpi=&time=&layerTimeOptions=&f=image")
+    image
   end
 end
+
+# http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export?bbox=47.607765%2C-122.333297%2C47.609747%2C-122.331580&bboxSR=4326&layers=7&layerdefs=&size=&imageSR=&format=png&transparent=false&dpi=&time=&layerTimeOptions=&f=image
