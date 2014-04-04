b278afad01f56f49aa58aea36315059f7011055f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-04 10:50:19 -0700

    refactor out uri

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,7 +9,8 @@ class Request
 
   def get_overlay
     bounds = {"bbox"=>self.bounds}.to_query
-    image = HTTParty.get("http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export?#{bounds}&bboxSR=4326&layers=7&layerdefs=&size=&imageSR=&format=png&transparent=false&dpi=&time=&layerTimeOptions=&f=image")
+    uri = "http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export"
+    image = HTTParty.get("#{uri}?#{bounds}&bboxSR=4326&layers=7&layerdefs=&size=&imageSR=&format=png&transparent=true&dpi=&time=&layerTimeOptions=&f=image")
 
     filename = "overlays/#{Time.now.to_i}.png"
 
