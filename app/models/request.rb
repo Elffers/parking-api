dcab22c8d713fbe86c271748c0fad5d50c21a70a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-07 11:27:03 -0700

    query string works

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,11 +9,12 @@ class Request
 
   def get_overlay
     uri = "http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export"
-    bounds = {"bbox"=>self.bounds}.to_query
-    spatial_reference = "4326"
-    image = HTTParty.get("#{uri}?#{bounds}&bboxSR=#{spatial_reference}&layers=7&layerdefs=&size=&imageSR=&format=png&transparent=true&dpi=&time=&layerTimeOptions=&f=image")
+    query = self.request_params_to_query
+    image = HTTParty.get("#{uri}?#{query}")
 
-    # http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export?bbox=47.607765%2C-122.333297%2C47.609747%2C-122.331580&bboxSR=4326&layers=7&layerdefs=&size=&imageSR=&format=png&transparent=true&dpi=&time=&layerTimeOptions=&f=image
+    p image.inspect
+
+    p "URI", "#{uri}?#{query}"
 
     filename = "overlays/#{Time.now.to_i}.png"
 
@@ -38,31 +39,31 @@ class Request
     end
   end
 
-#   def request_params
-#     layer = 7
-#     spatial_reference = 4326
-#     {
-#       "bbox"        => self.bounds,
-#       "bboxSR"      => spatial_reference,
-#       "layers"      => layer,
-#       "transparent" => "true",
-#       "format"      => "png",
-#     }.to_query
-# "bbox=47.607765%2C-122.333297%2C47.609747%2C-122.331580&bboxSR=4326&format=png&layers=7&transparent=true"
+  def request_params_to_query
+    layers = "7,6,8,9"
+    spatial_reference = 4326
+    size = '350,350'
 
+    {
+      "bbox"        => self.bounds,
+      "bboxSR"      => spatial_reference,
+      "layers"      => "show:#{layers}",
+      "f"           =>"image",
+      "size"        => size,
+      "transparent" => "true",
+    }.to_query
+  end
 
+# this one works:
+# http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export?bbox=47.607765%2C-122.333297%2C47.609747%2C-122.331580&bboxSR=4326&f=image&layers=show%3A7%2C6%2C8%2C9&size=&transparent=true
 
-dpi=96
-&transparent=true
-&format=png8
-&layers=show%3A7%2C5%2C6%2C8%2C9
-&bbox=1256880.4999999998%2C221219.3333333333%2C1301680.4999999998%2C233252.66666666663
-&bboxSR=2926
-&imageSR=2926
-&size=1344%2C361
-&f=image
+# {
+#     "bbox"=>"47.607765,-122.333297,47.609747,-122.331580",
+#     "bboxSR"=>"4326",
+#     "f"=>"image",
+#     "layers"=>"show:7,6,8,9",
+#     "size"=>nil,
+#     "transparent"=>"true"
+# }
 
-
-
-#   end
 end
