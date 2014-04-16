092ebad0d102b1474c32c7042c678db8af35a3bf
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-16 09:22:42 -0700

    read env variables directly from file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,11 @@
+fog_credentials = YAML.load(File.read("#{Rails.root}/config/application.yml"))
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => Figaro.env.aws_access_key_id,
-    :aws_secret_access_key  => Figaro.env.aws_secret_access_key
+    :aws_access_key_id      => fog_credentials['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => fog_credentials['AWS_SECRET_ACCESS_KEY']
   }
-  config.fog_directory  = Figaro.env.s3_bucket_name
+  config.fog_directory  = fog_credentials['S3_BUCKET_NAME']
   config.fog_public     = false
   # config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
