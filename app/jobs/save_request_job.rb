837a5156f3435fa61eba04db05da61c7fe2b48cf
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-18 16:57:41 -0700

    resquuueee

--- app/jobs/save_request_job.rb
+++ app/jobs/save_request_job.rb
@@ -1,9 +1,6 @@
 class SaveRequestJob
   @queue = :save_request
   def self.perform(request_params)
-    request = Request.create!(request_params)
-    # request.reset_url
-    # request.save
-    # return request
+    Request.create!(request_params)
   end
 end
