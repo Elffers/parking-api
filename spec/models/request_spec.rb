44a6aac66cf9c6d27ec3d820d926e1ac8d711360
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-18 15:49:53 -0700

    spec for matching overlay temp url

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -16,6 +16,12 @@ describe Request do
       expect(request.query).to_not be_nil
       expect(request.query).to eq "http://gisrevprxy.seattle.gov/ArcGIS/rest/services/SDOT_EXT/sdot_parking/MapServer/export?bbox=47.657289%2C-122.341331%2C47.693368%2C-122.310042&bboxSR=4326&dpi=96&f=image&format=png8&imageSR=2926&layers=show%3A7%2C6%2C8%2C9&size=400%2C400&transparent=true"
     end
+
+    it 'should point to temp file as overlay.url' do
+      request.get_overlay
+      id = /\d+-\d+-\d+/
+      expect(request.overlay.url).to match "/uploads/tmp/#{id}/export.png"
+    end
   end
 
   describe '.set_client' do
